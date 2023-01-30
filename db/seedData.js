6576689a9bbb10adc7b352369fd9763569ba3f4d
Kevin Paucar <kpaucar05@gmail.com>
2023-01-30 09:56:03 -0600

    db users

--- db/seedData.js
+++ db/seedData.js
@@ -3,14 +3,36 @@
 const client = require("./client")
 
 async function dropTables() {
+  try {
   console.log("Dropping All Tables...")
   // drop all tables, in the correct order
+  await client.query(`
+  DROP TABLE IF EXISTS mytablename;
+`);
+
+console.log("Finished dropping tables!");
+} catch (error) {
+console.error("Error dropping tables!");
+throw error;
+}
 }
 
 async function createTables() {
+  try {
   console.log("Starting to build tables...")
   // create all tables, in the correct order
+   await client.query(`
+    CREATE TABLE mytablename (
+      
+    )
+   `)
+   console.log("Finished building tables!");
+  } catch (error) {
+    console.error("Error building tables!");
+    throw error;
+  }
 }
+
 
 /* 
 
--- db/users.js
+++ db/users.js
@@ -4,7 +4,15 @@ const client = require("./client");
 
 // user functions
 async function createUser({ username, password }) {
-  
+  `CREATE TABLE users (
+    id SERIAL PRIMARY KEY 
+    username VARCHAR(255) UNIQUE NOT NULL
+    password VARCHAR(255) NOT NULL
+  )
+  `
+  const SALT_COUNT = 10;
+
+  const hashedPassword = await bcrypt.hash(password, SALT_COUNT)
 }
 
 async function getUser({ username, password }) {
