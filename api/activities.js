cf05e6239cfdfdce24cf89dcf06743c847c525cf
Ithndr <tm21999@gmail.com>
2023-02-09 16:39:30 -0500

    api users complete

--- api/activities.js
+++ api/activities.js
@@ -1,12 +1,24 @@
 const express = require('express');
 const router = express.Router();
 
-// GET /api/activities/:activityId/routines
-router.get('/:activityId/routines')
+
 // GET /api/activities
-router.get('/activities')
+router.get('/', async (req, res, next) => {
+    
+})
+
+// GET /api/activities/:activityId/routines
+router.get('/:activityId/routines', async (req, res, next) => {
+
+})
+
 // POST /api/activities
-router.post('/activites')
+router.post('/', async (req, res, next) => {
+
+})
 // PATCH /api/activities/:activityId
-router.patch(':activityId/routines')
+router.patch('/:activityId', async (req, res, next) => {
+
+})
+
 module.exports = router;
--- api/users.js
+++ api/users.js
@@ -2,50 +2,125 @@
 const express = require("express");
 const router = express.Router();
 const jwt = require('jsonwebtoken')
-const { getUserByUsername, createUser } = require('../db')
+const { getUserByUsername, createUser, getAllRoutinesByUser, getPublicRoutinesByUser } = require('../db')
+const { UserTakenError, PasswordTooShortError } = require('../errors')
+const bcrypt = require("bcrypt");
+
 
 // POST /api/users/register
-router.post('/register', async (req, res, next)=> {
-    const { username, password} = req.body;
-
-    try{
+router.post('/register', async (req, res, next) => {
+    const { username, password } = req.body;
+    try {
         const _user = await getUserByUsername(username)
-        if(_user){
-            next({
-                name: 'UserExistsError',
-                message: 'A user by that username already exists'
+        if (_user) {
+            res.send({
+                message: UserTakenError(username),
+                error: 'Username taken loser',
+                name: 'UsernameTakenError'
             })
-           }
-           if(password.length < 8){
-            throw new Error;
-           }
-        const user = await createUser({
-            username, password
-        });
+        }
+        if (password.length < 8) {
+            res.send({
+                message: PasswordTooShortError(username),
+                error: 'Password too short',
+                name: 'ShortPassError'
+
+            })
+        }
+        const user = await createUser({ username, password });
 
         const token = jwt.sign({
             id: user.id,
             username
-        }, process.env.JWT_SECRET,{
+        }, process.env.JWT_SECRET, {
             expiresIn: '1w'
         });
-    
-        res.send({ 
-          message: "thank you for signing up",
-          token 
+
+        res.send({
+            message: "thank you for signing up",
+            token,
+            user
         });
-      } catch ({ name, message }) {
-        next({ name, message })
-      } 
-    });
+    } catch (error) {
+        next(error)
+    }
+});
 
 // POST /api/users/login
-router.post('/login')
+router.post('/login', async (req, res, next) => {
+    const { username, password } = req.body;
+    try {
+        const user = await getUserByUsername(username);
+        const match = await bcrypt.compare(password, user.password)
+        if (!match) {
+            res.send({
+                name: 'PasswordMismatch',
+                message: 'Username or Password does not match'
+            })
+        }
+        const token = jwt.sign({
+            id: user.id,
+            username
+        }, process.env.JWT_SECRET, {
+            expiresIn: '1w'
+        });
+
+        res.send({
+            message: "you're logged in!",
+            token,
+            user
+        });
+    } catch (error) {
+        next(error);
+    }
+})
 
 // GET /api/users/me
-router.get('/me')
+router.get('/me', async (req, res, next) => {
+    try {
+        let token = req.header('Authorization');
+        if (!token) {
+            res.status(401).send({
+                error: 'NoToken',
+                message: "You must be logged in to perform this action",
+                name: 'NoTokenFound'
+            })
+        }
+        const newToken = token.slice(7)
+        const verifiedToken = jwt.verify(newToken, process.env.JWT_SECRET);
+        const user = await getUserByUsername(verifiedToken.username)
+        if(user){
+            res.send(user);
+        }
+    } catch (error) {
+        next(error);
+    }
+})
 
 // GET /api/users/:username/routines
-router.get('/:username.routines')
+router.get('/:username/routines', async (req, res, next) => {
+    const token = req.header('Authorization');
+    try {
+        const { username } = req.params;
+        if (!token) {
+            res.status(401).send({
+                error: 'NoToken',
+                message: "You must be logged in to perform this action",
+                name: 'NoTokenFound'
+            })
+        }
+        const newToken = token.slice(7)
+        const verifiedToken = jwt.verify(newToken, process.env.JWT_SECRET);
+        const user = await getUserByUsername(verifiedToken.username);
+        if(user.username === username){
+            const routines = await getAllRoutinesByUser({username:user.username})
+            res.send(routines)
+        }
+        const publicRoutines = await getPublicRoutinesByUser({username:username})
+        res.send(publicRoutines)
+    } catch (error) {
+        next(error);
+    }
+})
 
 module.exports = router;
