457ba8e6bbdcc72484e2aae3ee4436d0669e45e9
Ithndr <tm21999@gmail.com>
2023-02-11 23:06:42 -0500

    api routines patch

--- api/routines.js
+++ api/routines.js
@@ -2,18 +2,13 @@ const jwt = require('jsonwebtoken')
 const express = require('express');
 const router = express.Router();
 const { getAllPublicRoutines,
-    getRoutineById,
-    getAllRoutinesByUser,
-    getPublicRoutinesByActivity,
     updateRoutine,
-    destroyRoutine,
     createRoutine,
-    getUser,
-    getUserById,
     getUserByUsername,
     getPublicRoutinesByUser,
+    getAllRoutinesByUser
 } = require('../db');
-
+const { UnauthorizedUpdateError } = require('../errors')
 
 // GET /api/routines
 router.get('/', async (req, res, next) => {
@@ -29,7 +24,7 @@ router.get('/', async (req, res, next) => {
 router.post('/', async (req, res, next) => {
     const { isPublic, name, goal } = req.body;
     const token = req.header('Authorization');
-   
+
     try {
         if (!token) {
             res.send({
@@ -42,10 +37,10 @@ router.post('/', async (req, res, next) => {
         const verifiedToken = jwt.verify(newToken, process.env.JWT_SECRET);
         const user = await getUserByUsername(verifiedToken.username)
         const newRoutine = await createRoutine({
-            goal, 
+            goal,
             name,
             isPublic,
-           creatorId: user.id
+            creatorId: user.id
         });
 
 
@@ -56,50 +51,46 @@ router.post('/', async (req, res, next) => {
 });
 // PATCH /api/routines/:routineId
 router.patch('/:routineId', async (req, res, next) => {
-    const { goal, isPublic, name } = req.body;
+    const { name } = req.body;
     const { routineId } = req.params
-    console.log(req.params)
     const token = req.header('Authorization');
-    const newToken = token.slice(7)
-    const verifiedToken = jwt.verify(newToken, process.env.JWT_SECRET);
-    const user = await getUserByUsername(verifiedToken.username)
-  
     try {
-        if (!user) {
+        if (!token) {
             res.status(401).send({
                 error: 'NoToken',
                 message: 'You must be logged in to perform this action',
                 name: 'NoTokenFound',
             });
-           
         }
-
-        const routine = await  getPublicRoutinesByUser ({username: name})
-            console.log(routine)
+        const newToken = token.slice(7)
+        const verifiedToken = jwt.verify(newToken, process.env.JWT_SECRET);
+        const user = await getUserByUsername(verifiedToken.username)
+        const routine = await getAllRoutinesByUser({ username: user.username })
+        console.log(routine)
         if (!routine) {
-            res.status(404).send({
+            res.send({
                 error: 'RoutineNotFound',
                 message: `The routine with id does not exist`,
                 name: 'RoutineNotFound',
             });
-           
+
         }
 
-        if (!routine) {
+        if (user.username !== routine.creatorId) {
             res.status(403).send({
                 error: 'PermissionDenied',
-                message: 'You do not have permission ',
+                message: UnauthorizedUpdateError({username:user.username,name:name}),
                 name: 'PermissionDenied',
             });
         }
 
         const updatedRoutine = await updateRoutine({
-         id: routineId,
-         ...req.body,
-            
+            id: routineId,
+            ...req.body,
+
         });
-        if(!updateRoutine){
-            res.send({ 
+        if (!updateRoutine) {
+            res.send({
                 error: 'NoRoutine',
                 message: 'Can not find routine',
                 name: 'NoActivityError'
@@ -111,9 +102,9 @@ router.patch('/:routineId', async (req, res, next) => {
         next(error);
     }
 });
-    
-    
-    
+
+
+
 
 
 
