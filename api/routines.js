715291e3d1c5740b445426986808c732ca0da4c2
Kevin Paucar <kpaucar05@gmail.com>
2023-02-12 00:08:00 -0600

    almost

--- api/routines.js
+++ api/routines.js
@@ -6,7 +6,9 @@ const { getAllPublicRoutines,
     createRoutine,
     getUserByUsername,
     getPublicRoutinesByUser,
-    getAllRoutinesByUser
+    getAllRoutinesByUser,
+    getRoutineById,
+    destroyRoutine
 } = require('../db');
 const { UnauthorizedUpdateError } = require('../errors')
 
@@ -51,7 +53,6 @@ router.post('/', async (req, res, next) => {
 });
 // PATCH /api/routines/:routineId
 router.patch('/:routineId', async (req, res, next) => {
-    const { name } = req.body;
     const { routineId } = req.params
     const token = req.header('Authorization');
     try {
@@ -65,8 +66,7 @@ router.patch('/:routineId', async (req, res, next) => {
         const newToken = token.slice(7)
         const verifiedToken = jwt.verify(newToken, process.env.JWT_SECRET);
         const user = await getUserByUsername(verifiedToken.username)
-        const routine = await getAllRoutinesByUser({ username: user.username })
-        console.log(routine)
+        const routine = await getRoutineById(routineId);
         if (!routine) {
             res.send({
                 error: 'RoutineNotFound',
@@ -76,40 +76,69 @@ router.patch('/:routineId', async (req, res, next) => {
 
         }
 
-        if (user.username !== routine.creatorId) {
+        if (user.id !== routine.creatorId) {
             res.status(403).send({
-                error: 'PermissionDenied',
-                message: UnauthorizedUpdateError({username:user.username,name:name}),
-                name: 'PermissionDenied',
+              error: 'PermissionDenied',
+              message: `User ${user.username} is not allowed to update ${routine.name}`,
+              name: 'PermissionDenied',
             });
-        }
-
-        const updatedRoutine = await updateRoutine({
+          }
+      
+          const updatedRoutine = await updateRoutine({
             id: routineId,
             ...req.body,
-
-        });
-        if (!updateRoutine) {
-            res.send({
-                error: 'NoRoutine',
-                message: 'Can not find routine',
-                name: 'NoActivityError'
-            })
+          });
+      
+          if (!updatedRoutine) {
+            res.status(404).send({
+              error: 'NoRoutine',
+              message: 'Can not update routine',
+              name: 'NoRoutineFoundError',
+            });
+          }
+      
+          res.send(updatedRoutine);
+        } catch (error) {
+          next(error);
         }
+      });
+// DELETE /api/routines/:routineId
+router.delete('/:routineId', async (req, res, next) => {
+    const { routineId } = req.params;
+    const token = req.header('Authorization');
+console.log(routineId)
+    try {
+      if (!token) {
+        res.status(401).send({
+          error: 'NoToken',
+          message: 'You must be logged in to perform this action',
+          name: 'NoTokenFound',
+        });
+      }
 
-        res.send(updatedRoutine);
+      const newToken = token.slice(7);
+      const verifiedToken = jwt.verify(newToken, process.env.JWT_SECRET);
+      const user = await getUserByUsername(verifiedToken.username);
+      const routine = await getRoutineById(routineId);
+
+
+      if (user.id !== routine.creatorId) {
+        res.status(403).send({
+          error: 'PermissionDenied',
+          message: `User ${user.username} is not allowed to delete ${routine.name}`,
+          name: 'PermissionDenied',
+        });
+      }
+
+      const deletedRoutine = await destroyRoutine({ routineId });
+
+      
+      res.send(deletedRoutine);
     } catch (error) {
-        next(error);
+      next(error);
     }
 });
 
-
-
-
-
-
-// DELETE /api/routines/:routineId
-router.delete('/:routerId')
 // POST /api/routines/:routineId/activities
 router.post('/:routineId/activities')
 module.exports = router;
